ddc13f74334bcfdfda1514f5ddd0a2e28b692da1
Pratik Padalkar <padalkarpratik19@gmail.com>
2022-11-25 19:18:43 +0530

    Ready to deploy

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose')
-const mongoURI="mongodb://localhost:27017/notebookdata"
+
+const mongoURI="mongodb+srv://pratik:pratik@cluster0.6ucd66h.mongodb.net/notegenix?retryWrites=true&w=majority"
 // code for connection to database
 const connecttomongo =()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("Connected - Have fun")
-    })
+    mongoose.connect(mongoURI, {
+
+        useNewUrlParser: true, 
+        
+        useUnifiedTopology: true 
+        
+        }, err => {
+        if(err) throw err;
+        console.log('Connected to MongoDB!!!')
+        });
 }
 module.exports=connecttomongo;
